91e1a46c3d3fa5c8f7f5eef79eb5b5778a08acc5
Un8Jordan <laxmirathore554@gmail.com>
2025-08-06 18:13:57 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,17 @@
+def enterage(age):
+    if age < 0:
+        raise ValueError("Only positive values are allowed")
+    if age % 2 == 0:
+        print("Age is even")
+    else:
+        print("Age is odd")
+        try:
+            num = int(input("Enter your age:"))
+            enterage(num)
+
+       
+        except ValueError:
+            print("Only positive values are allowed!")
+
+        except:
+            print("An unexpected error occured.")
